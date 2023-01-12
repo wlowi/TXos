83784587fee3da2366cc5e580efe900e6b480f97
Wolfgang Lohwasser <wolfgang.lohwasser@web.de>
2023-01-12 08:58:29 +0100

    Copyright messages

--- src/ui/TableEditable.h
+++ src/ui/TableEditable.h
@@ -1,5 +1,28 @@
 /*
+    TXos. A remote control transmitter OS.
+
+    Copyright (C) 2022 Wolfgang Lohwasser
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+/*
  * Interface TableEditable
+ *
+ * Classes derived from TableEditable can be displayed and edited in
+ * a table like user interface.
  */
 
 #ifndef _TableEditable_h_
@@ -37,6 +60,8 @@ typedef struct CellType_t {
 
 } CellType_t;
 
+/* A cell of a editable table.
+ */
 class Cell {
 
     private:
